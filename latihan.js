e31f0b2b6e612a288402750560e3a07463095f4c
Muizzuddin-github <muizzuddin334@gmail.com>
2021-06-14 07:40:54 +0700

    initial commit

---
+++ latihan.js
@@ -0,0 +1,22 @@
+// alert('Hai...');
+
+// for( var nilaiAwal = true; nilaiAwal === true; nilaiAwal = confirm('jawab?')){
+
+
+//     var nilaiUser = prompt('masukkan jawaban');
+
+//     switch(nilaiUser){
+//         case 'ada apa' :
+//             alert('kamu sedang apa?');
+//         break;
+//         case 'sedang mengerjakan sesuatu' :
+//             alert('sesuatu apa?');
+//         break;
+//         case 'mau lihat?' :
+//             alert('coba lihat!');
+//         break;
+//         default:
+//             alert('baca naskahnya!');
+//         break;
+//     }
+// }
